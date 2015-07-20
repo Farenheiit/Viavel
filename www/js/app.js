235ade6af7a185790a6b3b5f53cf9183420c8b47
Antônio Paulo <apaulo2007@gmail.com>
2015-07-20 20:25:22 -0300

    inclusão do controlador MapController angular no app.js - 0001.

--- www/js/app.js
+++ www/js/app.js
@@ -19,11 +19,29 @@ angular.module('starter', [
       StatusBar.styleDefault();
     }
   });
-}).controller('TodoCtrl', function($scope) {
-  $scope.tasks = [
-    { title: 'Collect coins' },
-    { title: 'Eat mushrooms' },
-    { title: 'Get high enough to grab the flag' },
-    { title: 'Find the Princess' }
-  ];
+}).controller('MapController', function($scope, $ionicLoading) {
+ 
+    google.maps.event.addDomListener(window, 'load', function() {
+        var myLatlng = new google.maps.LatLng(37.3000, -120.4833);
+ 
+        var mapOptions = {
+            center: myLatlng,
+            zoom: 16,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+ 
+        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+ 
+        navigator.geolocation.getCurrentPosition(function(pos) {
+            map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+            var myLocation = new google.maps.Marker({
+                position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+                map: map,
+                title: "My Location"
+            });
+        });
+ 
+        $scope.map = map;
+    });
+ 
 });
