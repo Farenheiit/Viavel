4f2e9e86e7120648893254abc1f8d85633de7e10
Antônio Paulo <apaulo2007@gmail.com>
2015-07-21 21:51:09 -0300

    separação do controlador do roteamento da aplicação - 0003.

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,25 +1,7 @@
-// Ionic Starter App
+angular.module('starter.controllers', [])
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', [
-  'ionic'
-
-])
-
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-}).controller('MapController', function($scope, $ionicLoading) {
+// Inicio do controlador MapController
+.controller('MapController', function($scope, $ionicLoading) {
         'use strict';
         // declaração de variaveis globais
         var geocoder;
@@ -147,4 +129,7 @@ angular.module('starter', [
         
         $scope.map = map;
         google.maps.event.addDomListener(window, 'load', initialize);
-});
+}); // fim do controlador MapController
+	
+
+
